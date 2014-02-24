289725c3b42dd05fd404cb4a324a45880fea3d06
Chad Mason <masonc2011@my.fit.edu>
2014-02-24 11:16:20 -0500

    Added a few changes.

--- RailRoadSignal/RailRoadSignal/RailRoadSignal/TrackSegment.cs
+++ RailRoadSignal/RailRoadSignal/RailRoadSignal/TrackSegment.cs
@@ -9,21 +9,16 @@ namespace RailRoadSignal
         /// <summary>
         /// Default constructor for a new track Segment
         /// </summary>
-        public TrackSegment(): base()
-        {
-             
-        }
-
+        public TrackSegment()
+            : base() { }
         /// <summary>
         /// Constructor for a new line
         /// Sets the thickness to 5f, and deoth to 1.0f
         /// </summary>
         /// <param name="_a">End point for the line</param>
         /// <param name="_b">End point for the line</param>
-        public TrackSegment(Vector2 _a, Vector2 _b): base(_a, _b)
-        { 
-
-        }
+        public TrackSegment(Vector2 _a, Vector2 _b)
+            : base(_a, _b) { }
         /// <summary>
         /// Constructor for a new line
         /// </summary>
@@ -32,10 +27,14 @@ namespace RailRoadSignal
         /// <param name="_thickness">The thickness to draw the line</param>
         /// <param name="_depth">The sorting depth of the sprite, between 0 (front) and 1 (back).</param>
         public TrackSegment(Vector2 _a, Vector2 _b, float _thickness, float _depth)
-            :base (_a, _b, _thickness, _depth)
-        { 
+            : base(_a, _b, _thickness, _depth) { }
 
-        }
-         
+
+        //**************************************
+        /* Add additional constructors here */
+        //**************************************
+
+
+
     }
 }
