81c8ad070ac5380b00e8fac84424ffaffa0daef8
Ken <topgun122104@yahoo.com>
2014-03-04 13:26:59 -0500

    Updated SBD calculation

--- Calculation Algs/GE Algorithms/GE Algorithms/Algorithms.cs
+++ Calculation Algs/GE Algorithms/GE Algorithms/Algorithms.cs
@@ -31,32 +31,61 @@ namespace GE_Algorithms
 {
     class Algorithms
     {
-        static const double MPH_TO_FPS = 1.467;
-        static const double FPS_TO_MPH = 0.7333;
-
-        static double SafeBrakingDistanceCalc(double cabCommandSpeed, double grade)
+        static double SafeBrakingDistanceCalc(int brakeLocation, int targetLocation, double gradeWorst,
+                                              double speedMax, double overspeed, double vehicleAccel,
+                                              double reactionTime, double brakeRate, double runwayAccelSec,
+                                              double propulsionRemSec, int brakeBuildUpSec, int overhangDist,
+                                              double safetyFact)
         {
-            //Vos is assumed to be cab command speed + 4mph overspeed
-            double Vos = cabCommandSpeed + 4.0; // in units: MPH
-            double RTmax = 3.8; // in units: seconds
-            //Final velocity when braking will be zero
-            double vF = 0;
-            //Used to adjust braking distance for effects of grade
-            double correctionFactor = 0.22;
-            //De-rated brake rate
-            double BReff = 1.95; //in units: MPHPS
-            //35% safety factor to be applied to braking portion of calculation only
-            double safetyFactor = 1.35;
-            //Vehicle overhang is 7' on each end
-            double overHang = 14; //in units: Feet
-
-            //Actual safe braking distance
-            double SBD = (Vos * RTmax * MPH_TO_FPS) + ((Math.Pow(Vos, 2) - Math.Pow(vF, 2))
-                * (FPS_TO_MPH / (correctionFactor * grade + BReff))) * safetyFactor + overHang;
-
+            int availStopDist = Math.Abs(targetLocation - brakeLocation);
+            //Console.WriteLine(availStopDist);
+            double overspeedTotal = overspeed + speedMax;
+            //Console.WriteLine(overspeedTotal);
+            double velocityFinal = (vehicleAccel * 1.2) + overspeedTotal;
+            //Console.WriteLine(velocityFinal);
+            double reactionDist = reactionTime * overspeedTotal * 1.467;
+            //Console.WriteLine(reactionDist);
+            double negGradeAdjBrakeDist = 0.733 * velocityFinal * velocityFinal / (brakeRate + (0.219 * gradeWorst));
+            //Console.WriteLine(negGradeAdjBrakeDist);
+            double posGradeAdjBrakeDist = 0.733 * velocityFinal * velocityFinal / (brakeRate - (0.219 * gradeWorst));
+            //Console.WriteLine(posGradeAdjBrakeDist);
+            double runwayAccelFt = 1.467 * (0.5 * vehicleAccel * runwayAccelSec + overspeedTotal) * runwayAccelSec;
+            //Console.WriteLine(runwayAccelFt);
+            double propulsionRemFt = velocityFinal * 1.467 * propulsionRemSec;
+            //Console.WriteLine(propulsionRemFt);
+            double brakeBuildUpFt = velocityFinal * 1.467 * brakeBuildUpSec;
+            //Console.WriteLine(brakeBuildUpFt);
+            double SBD = Math.Round(reactionDist + runwayAccelFt + propulsionRemFt + brakeBuildUpFt + negGradeAdjBrakeDist + overhangDist, 1);
             return SBD;
+        }
 
-
+        static int Main(string[] args)
+        {
+            double test1 = SafeBrakingDistanceCalc(48895, 49485, 1.296, 15.0, 1.0, 2.31, 4.8, 1.67, 1.2, 0.5, 1, 28, 0);
+            Console.WriteLine(test1);
+            double test2 = SafeBrakingDistanceCalc(48895, 50100, 2.467, 35.0, 2.0, 1.6007, 4.8, 1.67, 1.2, 0.5, 1, 28, 0);
+            Console.WriteLine(test2);
+            double test3 = SafeBrakingDistanceCalc(49485, 50100, 3.59, 20.0, 1.0, 2.31, 4.8, 1.67, 1.2, 0.5, 1, 28, 0);
+            Console.WriteLine(test3);
+            double test4 = SafeBrakingDistanceCalc(49485, 51066, 1.879, 45.0, 2.0, 1.0565, 4.8, 1.67, 1.2, 0.5, 1, 28, 0);
+            Console.WriteLine(test4);
+            double test5 = SafeBrakingDistanceCalc(49485, 52032, -0.405, 55.0, 2.0, 0.6079, 4.8, 1.67, 1.2, 0.5, 1, 28, 0);
+            Console.WriteLine(test5);
+            double test6 = SafeBrakingDistanceCalc(50100, 51066, 0.7902, 25.0, 2.0, 2.2406, 4.8, 1.67, 1.2, 0.5, 1, 28, 0);
+            Console.WriteLine(test6);
+            double test7 = SafeBrakingDistanceCalc(50100, 52032, -1.677, 35.0, 2.0, 1.6007, 4.8, 1.67, 1.2, 0.5, 1, 28, 0);
+            Console.WriteLine(test7);
+            double test8 = SafeBrakingDistanceCalc(50100, 52998, -0.962, 55.0, 2.0, 0.6079, 4.8, 1.67, 1.2, 0.5, 1, 28, 0);
+            Console.WriteLine(test8);
+            double test9 = SafeBrakingDistanceCalc(51066, 52998, -1.837, 35.0, 2.0, 1.6007, 4.8, 1.67, 1.2, 0.5, 1, 28, 0);
+            Console.WriteLine(test9);
+            double test10 = SafeBrakingDistanceCalc(51066, 53964, -2.032, 45.0, 2.0, 1.0565, 4.8, 1.67, 1.2, 0.5, 1, 28, 0);
+            Console.WriteLine(test10);
+            double test11 = SafeBrakingDistanceCalc(51066, 54930, -0.819, 65.0, 2.0, 0.3471, 4.8, 1.67, 1.2, 0.5, 1, 28, 0);
+            Console.WriteLine(test11);
+            return 0;
         }
     }
 }
+
+
