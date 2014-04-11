6b174a48514c6ad6c39b25cea606b71cb80566f6
Chris Diebold <cdiebold2012@my.fit.edu>
2014-04-11 12:28:26 -0400

    insert function for track segment

--- RailRoadSignal/RailRoadSignal/RailRoadSignal/Database/DatabaseOerations.cs
+++ RailRoadSignal/RailRoadSignal/RailRoadSignal/Database/DatabaseOerations.cs
@@ -14,16 +14,26 @@ namespace RailRoadSignal.Database
     {
         private void insertIntoDb(TrackSegment obj)
         {
+            MySqlCommand cmd = new MySqlCommand();
             DatabaseConnection conn = new DatabaseConnection("andrew.cs.fit.edu", 3306, "signalblockdesign", "signalblockdesig", "E2SnzbV922m6R51");
-            Query q = new Query();
-            StringBuilder sb = new StringBuilder();
-            sb.Append("insert into TrackSegment (segmenNumber, grade, )");
-            /*try
-            {
-                conn.openConnection();
-
-            }
-             */
+            conn.openConnection();
+            cmd.CommandText = "Insert into trackSegment(trackCircuit, trackNumber, fromDist, toDist, worstCaseGrade, maxEntrySpeed, overSpeed, vehicleAccel, reactionTime, brakeRate, runwayAccelSec, propulsion, buildUpBrake, overhangDist) values(@trackCircuit, @trackNumber, @fromDist, @toDist, @worstCaseGrade, @maxEntrySpeed, @overSpeed, @vehicleAccel, @reactionTime, @brakeRate, @runwayAccelSec, @propulsion, @buildUpBrake, @overhangDist)";
+            cmd.Parameters.AddWithValue("@trackCircuit", obj.TrackCircuit);
+            cmd.Parameters.AddWithValue("@trackNumber", obj.TrackID);
+            cmd.Parameters.AddWithValue("@fromDist", obj.StartPoint);
+            cmd.Parameters.AddWithValue("@toDist", obj.EndPoint);
+            cmd.Parameters.AddWithValue("@worstCaseGrade", obj.GradeWorst);
+            cmd.Parameters.AddWithValue("@maxEntrySpeed", obj.SpeedMax);
+            cmd.Parameters.AddWithValue("@overSpeed", obj.OverSpeed);
+            cmd.Parameters.AddWithValue("@vehicleAccel", obj.VehicleAccel);
+            cmd.Parameters.AddWithValue("@reactionTime", obj.ReactionTime);
+            cmd.Parameters.AddWithValue("@brakeRate", obj.BrakeRate);
+            cmd.Parameters.AddWithValue("@runwayAccelSec", obj.RunwayAccelSec);
+            cmd.Parameters.AddWithValue("@propulsion", obj.PropulsionRemSec);
+            cmd.Parameters.AddWithValue("@buildUpBrake", obj.BrakeBuildUpSec);
+            cmd.Parameters.AddWithValue("@overhangDist", obj.OverhangDist);
+            cmd.ExecuteNonQuery();
+            conn.closeConnection();
 
 
         }
