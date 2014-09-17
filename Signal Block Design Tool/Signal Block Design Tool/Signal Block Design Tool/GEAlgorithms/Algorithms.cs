bb3fb61f118a703383eb9f081b953b82fb38c811
Ken <topgun122104@yahoo.com>
2014-09-17 16:08:28 -0400

    Changed Algorithm

--- Signal Block Design Tool/Signal Block Design Tool/Signal Block Design Tool/GEAlgorithms/Algorithms.cs
+++ Signal Block Design Tool/Signal Block Design Tool/Signal Block Design Tool/GEAlgorithms/Algorithms.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Windows.Forms;
 
 namespace Signal_Block_Design_Tool.GEAlgorithms
 {
@@ -33,7 +37,7 @@ namespace Signal_Block_Design_Tool.GEAlgorithms
                                               double propulsionRemSec, int brakeBuildUpSec, int overhangDist,
                                               double safetyFact)
         {
-            int availStopDist = Math.Abs(targetLocation - brakeLocation);
+            double availStopDist = Math.Abs(targetLocation - brakeLocation);
             //Console.WriteLine(availStopDist);
             double overspeedTotal = overspeed + speedMax;
             //Console.WriteLine(overspeedTotal);
@@ -52,13 +56,15 @@ namespace Signal_Block_Design_Tool.GEAlgorithms
             double brakeBuildUpFt = velocityFinal * 1.467 * brakeBuildUpSec;
             //Console.WriteLine(brakeBuildUpFt);
             double SBD = Math.Round(reactionDist + runwayAccelFt + propulsionRemFt + brakeBuildUpFt + negGradeAdjBrakeDist + overhangDist, 1);
+            bool isSafe = SBD < availStopDist;
+            if (!isSafe)
+            {
+                 string response = "This is an unsafe condition!\n\n There are " + Math.Abs(SBD - availStopDist) + 
+                      " feet lees then required for a safe breaking condition";
+                 MessageBox.Show(response, "Cirtical Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                 return SBD;
+            }
             return SBD;
-        }
-
-
-        public static double HeadwayCalculations()
-        {
-            return 0;
         }
 
 
@@ -66,49 +72,36 @@ namespace Signal_Block_Design_Tool.GEAlgorithms
         {
             return 0;
         }
-
-
-        public static double ClearTimeCalculation()
-        {
-            return 0;
-        }
-
-
-        public static double ApproachLockingTimeCalculations()
-        {
-            return 0;
-        }
-
         /// <sumary>
         /// Test main for safe breaking distance calculations
         /// </summary>
-        /*static int Main(string[] args)
+        static int Main(string[] args)
         {
-            double test1 = SafeBrakingDistanceCalc(48895, 49485, 1.296, 15.0, 1.0, 2.31, 4.8, 1.67, 1.2, 0.5, 1, 28, 0);
+             double test1 = SafeBrakingDistanceCalculations(48895, 49485, 1.296, 15.0, 1.0, 2.31, 4.8, 1.67, 1.2, 0.5, 1, 28, 0);
             Console.WriteLine(test1);
-            double test2 = SafeBrakingDistanceCalc(48895, 50100, 2.467, 35.0, 2.0, 1.6007, 4.8, 1.67, 1.2, 0.5, 1, 28, 0);
+            double test2 = SafeBrakingDistanceCalculations(48895, 50100, 2.467, 35.0, 2.0, 1.6007, 4.8, 1.67, 1.2, 0.5, 1, 28, 0);
             Console.WriteLine(test2);
-            double test3 = SafeBrakingDistanceCalc(49485, 50100, 3.59, 20.0, 1.0, 2.31, 4.8, 1.67, 1.2, 0.5, 1, 28, 0);
+            double test3 = SafeBrakingDistanceCalculations(49485, 50100, 3.59, 20.0, 1.0, 2.31, 4.8, 1.67, 1.2, 0.5, 1, 28, 0);
             Console.WriteLine(test3);
-            double test4 = SafeBrakingDistanceCalc(49485, 51066, 1.879, 45.0, 2.0, 1.0565, 4.8, 1.67, 1.2, 0.5, 1, 28, 0);
+            double test4 = SafeBrakingDistanceCalculations(49485, 51066, 1.879, 45.0, 2.0, 1.0565, 4.8, 1.67, 1.2, 0.5, 1, 28, 0);
             Console.WriteLine(test4);
-            double test5 = SafeBrakingDistanceCalc(49485, 52032, -0.405, 55.0, 2.0, 0.6079, 4.8, 1.67, 1.2, 0.5, 1, 28, 0);
+            double test5 = SafeBrakingDistanceCalculations(49485, 52032, -0.405, 55.0, 2.0, 0.6079, 4.8, 1.67, 1.2, 0.5, 1, 28, 0);
             Console.WriteLine(test5);
-            double test6 = SafeBrakingDistanceCalc(50100, 51066, 0.7902, 25.0, 2.0, 2.2406, 4.8, 1.67, 1.2, 0.5, 1, 28, 0);
+            double test6 = SafeBrakingDistanceCalculations(50100, 51066, 0.7902, 25.0, 2.0, 2.2406, 4.8, 1.67, 1.2, 0.5, 1, 28, 0);
             Console.WriteLine(test6);
-            double test7 = SafeBrakingDistanceCalc(50100, 52032, -1.677, 35.0, 2.0, 1.6007, 4.8, 1.67, 1.2, 0.5, 1, 28, 0);
+            double test7 = SafeBrakingDistanceCalculations(50100, 52032, -1.677, 35.0, 2.0, 1.6007, 4.8, 1.67, 1.2, 0.5, 1, 28, 0);
             Console.WriteLine(test7);
-            double test8 = SafeBrakingDistanceCalc(50100, 52998, -0.962, 55.0, 2.0, 0.6079, 4.8, 1.67, 1.2, 0.5, 1, 28, 0);
+            double test8 = SafeBrakingDistanceCalculations(50100, 52998, -0.962, 55.0, 2.0, 0.6079, 4.8, 1.67, 1.2, 0.5, 1, 28, 0);
             Console.WriteLine(test8);
-            double test9 = SafeBrakingDistanceCalc(51066, 52998, -1.837, 35.0, 2.0, 1.6007, 4.8, 1.67, 1.2, 0.5, 1, 28, 0);
+            double test9 = SafeBrakingDistanceCalculations(51066, 52998, -1.837, 35.0, 2.0, 1.6007, 4.8, 1.67, 1.2, 0.5, 1, 28, 0);
             Console.WriteLine(test9);
-            double test10 = SafeBrakingDistanceCalc(51066, 53964, -2.032, 45.0, 2.0, 1.0565, 4.8, 1.67, 1.2, 0.5, 1, 28, 0);
+            double test10 = SafeBrakingDistanceCalculations(51066, 53964, -2.032, 45.0, 2.0, 1.0565, 4.8, 1.67, 1.2, 0.5, 1, 28, 0);
             Console.WriteLine(test10);
-            double test11 = SafeBrakingDistanceCalc(51066, 54930, -0.819, 65.0, 2.0, 0.3471, 4.8, 1.67, 1.2, 0.5, 1, 28, 0);
+            double test11 = SafeBrakingDistanceCalculations(51066, 54930, -0.819, 65.0, 2.0, 0.3471, 4.8, 1.67, 1.2, 0.5, 1, 28, 0);
             Console.WriteLine(test11);
             return 0;
         }
-         */
+         
     }
 }
 
