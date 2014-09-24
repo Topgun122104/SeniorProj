824485767dd230c57c3f610308b16f560e5ac076
Ken <topgun122104@yahoo.com>
2014-09-24 16:28:38 -0400

    Alg Mod

--- Signal Block Design Tool/Signal Block Design Tool/Signal Block Design Tool/GEAlgorithms/Algorithms.cs
+++ Signal Block Design Tool/Signal Block Design Tool/Signal Block Design Tool/GEAlgorithms/Algorithms.cs
@@ -34,8 +34,7 @@ namespace Signal_Block_Design_Tool.GEAlgorithms
         public static double SafeBrakingDistanceCalculations(int brakeLocation, int targetLocation, double gradeWorst,
                                               double speedMax, double overspeed, double vehicleAccel,
                                               double reactionTime, double brakeRate, double runwayAccelSec,
-                                              double propulsionRemSec, int brakeBuildUpSec, int overhangDist,
-                                              double safetyFact)
+                                              double propulsionRemSec, int brakeBuildUpSec, int overhangDist)
         {
             double availStopDist = Math.Abs(targetLocation - brakeLocation);
             //Console.WriteLine(availStopDist);
@@ -77,27 +76,27 @@ namespace Signal_Block_Design_Tool.GEAlgorithms
         /// </summary>
         /*static int Main(string[] args)
         {
-             double test1 = SafeBrakingDistanceCalculations(48895, 49485, 1.296, 15.0, 1.0, 2.31, 4.8, 1.67, 1.2, 0.5, 1, 28, 0);
+             double test1 = SafeBrakingDistanceCalculations(48895, 49485, 1.296, 15.0, 1.0, 2.31, 4.8, 1.67, 1.2, 0.5, 1, 28);
             Console.WriteLine(test1);
-            double test2 = SafeBrakingDistanceCalculations(48895, 50100, 2.467, 35.0, 2.0, 1.6007, 4.8, 1.67, 1.2, 0.5, 1, 28, 0);
+            double test2 = SafeBrakingDistanceCalculations(48895, 50100, 2.467, 35.0, 2.0, 1.6007, 4.8, 1.67, 1.2, 0.5, 1, 28);
             Console.WriteLine(test2);
-            double test3 = SafeBrakingDistanceCalculations(49485, 50100, 3.59, 20.0, 1.0, 2.31, 4.8, 1.67, 1.2, 0.5, 1, 28, 0);
+            double test3 = SafeBrakingDistanceCalculations(49485, 50100, 3.59, 20.0, 1.0, 2.31, 4.8, 1.67, 1.2, 0.5, 1, 28);
             Console.WriteLine(test3);
-            double test4 = SafeBrakingDistanceCalculations(49485, 51066, 1.879, 45.0, 2.0, 1.0565, 4.8, 1.67, 1.2, 0.5, 1, 28, 0);
+            double test4 = SafeBrakingDistanceCalculations(49485, 51066, 1.879, 45.0, 2.0, 1.0565, 4.8, 1.67, 1.2, 0.5, 1, 28);
             Console.WriteLine(test4);
-            double test5 = SafeBrakingDistanceCalculations(49485, 52032, -0.405, 55.0, 2.0, 0.6079, 4.8, 1.67, 1.2, 0.5, 1, 28, 0);
+            double test5 = SafeBrakingDistanceCalculations(49485, 52032, -0.405, 55.0, 2.0, 0.6079, 4.8, 1.67, 1.2, 0.5, 1, 28);
             Console.WriteLine(test5);
-            double test6 = SafeBrakingDistanceCalculations(50100, 51066, 0.7902, 25.0, 2.0, 2.2406, 4.8, 1.67, 1.2, 0.5, 1, 28, 0);
+            double test6 = SafeBrakingDistanceCalculations(50100, 51066, 0.7902, 25.0, 2.0, 2.2406, 4.8, 1.67, 1.2, 0.5, 1, 28);
             Console.WriteLine(test6);
-            double test7 = SafeBrakingDistanceCalculations(50100, 52032, -1.677, 35.0, 2.0, 1.6007, 4.8, 1.67, 1.2, 0.5, 1, 28, 0);
+            double test7 = SafeBrakingDistanceCalculations(50100, 52032, -1.677, 35.0, 2.0, 1.6007, 4.8, 1.67, 1.2, 0.5, 1, 28);
             Console.WriteLine(test7);
-            double test8 = SafeBrakingDistanceCalculations(50100, 52998, -0.962, 55.0, 2.0, 0.6079, 4.8, 1.67, 1.2, 0.5, 1, 28, 0);
+            double test8 = SafeBrakingDistanceCalculations(50100, 52998, -0.962, 55.0, 2.0, 0.6079, 4.8, 1.67, 1.2, 0.5, 1, 28);
             Console.WriteLine(test8);
-            double test9 = SafeBrakingDistanceCalculations(51066, 52998, -1.837, 35.0, 2.0, 1.6007, 4.8, 1.67, 1.2, 0.5, 1, 28, 0);
+            double test9 = SafeBrakingDistanceCalculations(51066, 52998, -1.837, 35.0, 2.0, 1.6007, 4.8, 1.67, 1.2, 0.5, 1, 28);
             Console.WriteLine(test9);
-            double test10 = SafeBrakingDistanceCalculations(51066, 53964, -2.032, 45.0, 2.0, 1.0565, 4.8, 1.67, 1.2, 0.5, 1, 28, 0);
+            double test10 = SafeBrakingDistanceCalculations(51066, 53964, -2.032, 45.0, 2.0, 1.0565, 4.8, 1.67, 1.2, 0.5, 1, 28);
             Console.WriteLine(test10);
-            double test11 = SafeBrakingDistanceCalculations(51066, 54930, -0.819, 65.0, 2.0, 0.3471, 4.8, 1.67, 1.2, 0.5, 1, 28, 0);
+            double test11 = SafeBrakingDistanceCalculations(51066, 54930, -0.819, 65.0, 2.0, 0.3471, 4.8, 1.67, 1.2, 0.5, 1, 28);
             Console.WriteLine(test11);
             return 0;
         }
